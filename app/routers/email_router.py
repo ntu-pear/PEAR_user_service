381dd9769121333d26e05687b2302bcac0c4e8ab
weiyan <weiyanwy@hotmail.com>
2024-12-20 00:10:12 +0800

    Update email_router.py

--- app/routers/email_router.py
+++ app/routers/email_router.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
 from ..service.email_service import generate_confirmation_token, confirm_token,send_confirmation_email
+
 from sqlalchemy.orm import Session
 from app.database import get_db
 from app.models.user_model import User
@@ -22,11 +23,13 @@ async def request_email_confirmation(user_id: int, db: Session = Depends(get_db)
 @router.get("/confirm-email/{token}")
 async def confirm_email(token: str, db: Session = Depends(get_db)):
     try:
-        email = confirm_token(token)
+        userDetails = confirm_token(token)
+       
     except:
         raise HTTPException(status_code=400, detail="Invalid or expired token")
     
-    user = db.query(User).filter(User.email == email).first()
+    user = db.query(User).filter(User.email == userDetails.get("email")).first()
+    #return user
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
     
@@ -34,3 +37,15 @@ async def confirm_email(token: str, db: Session = Depends(get_db)):
     db.commit()
     
     return {"msg": "Email confirmed"}
+
+# Request Email Confirmation
+#@router.post("/request-reset-password/")
+#async def request_reset_confirmation(user_email: str, db: Session = Depends(get_db)):
+#    user = db.query(User).filter(User.email == user_email).first()
+#    if not user:
+#        raise HTTPException(status_code=404, detail="User not found")
+    
+#    token = generate_confirmation_token(user.email)
+#    await send_resetpassword_email(user.email, token)
+    # await send_email("test1","test2",user.email)
+#    return {"msg": "Reset password email sent"}
