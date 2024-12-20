3f1eaa2e645ca2cf6b07f3b93434bfebf8ed954a
weiyanwy <84122998+weiyanwy@users.noreply.github.com>
2024-12-20 20:56:19 +0800

    Change Login, username to email

--- app/routers/user_auth_router.py
+++ app/routers/user_auth_router.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException,status
+
 from sqlalchemy.orm import Session
 from fastapi.security import OAuth2PasswordRequestForm
 import json
@@ -15,16 +16,21 @@ router = APIRouter()
 
 @router.post("/login")#, response_model=user_auth.Token)
 def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
-    user = db.query(User).filter(User.userName == form_data.username).first()
+    #Get User
+    user = db.query(User).filter(User.email == form_data.email).first()
     
     if not user:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="The user does not exist")
     if not user_auth_service.verify_password(form_data.password, user.passwordHash):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
-    role=get_user_role_by_user(db=db,user_id=user.id)
     
+    #Get user roleID
+    role=get_user_role_by_user(db=db,user_id=user.id)
+
     data={"userId":user.id,"roleId":role.roleId}
+    #Convert to JSON
     data=json.dumps(data)
+    #Add data into access token
     access_token = user_auth_service.create_access_token(data={"sub": data})
     return {"access_token": access_token, "token_type": "bearer"}
 
