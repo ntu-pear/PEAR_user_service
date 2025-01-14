65150640df270177c294302236c63657c116f124
choozhenhui <46195268+choozhenhui@users.noreply.github.com>
2025-01-14 21:20:02 +0800

    uuid generation

--- create_db.py
+++ create_db.py
@@ -5,55 +5,31 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
-# # Get individual components from environment variables
-# driver = os.getenv("DB_DRIVER")
-# server = os.getenv("DB_SERVER")
-# database = 'master'  # Connect to the master database to create a new database
-# username = os.getenv("DB_USERNAME")
-# password = os.getenv("DB_PASSWORD")
-
-# # Construct the connection string
-# connection_string = f'DRIVER={{{driver}}};SERVER={{{server}}};DATABASE={database};UID={username};PWD={password}'
-# DB_DATABASE = os.getenv("DB_DATABASE")
-# # SQL command to create the database (change the db name if required)
-# create_db_query = f"IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = '{DB_DATABASE}') CREATE DATABASE FYP;"
-
-# try:
-#     # Connect to the SQL Server
-#     with pyodbc.connect(
-#         driver=f'{{{driver}}}',
-#         server=server,
-#         database=database,
-#         uid=username,
-#         pwd=password,
-#         autocommit=True,
-#     ) as conn:
-#         with conn.cursor() as cursor:
-#             cursor.execute(create_db_query)
-#             print(f"Database '{DB_DATABASE}' created or already exists.")
-# except Exception as e:
-#     print(f"An error occurred: {e}")
-    
-
 # Get individual components from environment variables
 driver = os.getenv("DB_DRIVER")
-server = os.getenv("DB_SERVER")
+#ntu
+#server = os.getenv("DB_SERVER")
+#local
+server = os.getenv("DB_SERVER_DEV")
+DB_DATABASE = os.getenv("DB_DATABASE")
 username = os.getenv("DB_USERNAME")
 password = os.getenv("DB_PASSWORD")
 
 # Construct the connection string
 #connection_string = f'DRIVER={{{driver}}};SERVER={{{server}}};DATABASE={database};UID={username};PWD={password}'
-DB_DATABASE = os.getenv("DB_DATABASE")
 # SQL command to create the database (change the db name if required)
 create_db_query = f"IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = '{DB_DATABASE}') CREATE DATABASE [{DB_DATABASE}];"
 
 try:
-    connection_string = f"DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password};"
+    #ntu
+    #connection_string = f"DRIVER={driver};SERVER={server};DATABASE={DB_DATABASE};UID={username};PWD={password};"
+    #local
+    connection_string = (f"DRIVER={{{driver}}};"f"SERVER={server};"f"DATABASE={DB_DATABASE};"f"Trusted_Connection=yes;")
     with pyodbc.connect(connection_string, autocommit=True) as conn:
         with conn.cursor() as cursor:
             cursor.execute(create_db_query)
-            print(f"Database {database} created or already exists.")
+            print(f"Database '{DB_DATABASE}' created or already exists.")
 except Exception as e:
     print("Error details:")
-    print(f"Driver: {driver}, Server: {server}, Database: {database}, User: {username}")
+    print(f"Driver: {driver}, Server: {server}, Database: {DB_DATABASE}, User: {username}")
     print(f"An error occurred: {e}")
--- init_db.py
+++ init_db.py
@@ -1,7 +1,7 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.exc import ProgrammingError
-#from app.database import Base
+from app.database import Base
 from dotenv import load_dotenv
 from app.models import privacy_level_setting_model,role_model,secret_question_model,user_model,user_role_model,user_secret_question_model
 
@@ -10,9 +10,14 @@ from app.models import privacy_level_setting_model,role_model,secret_question_mo
 load_dotenv()
 
 # Get the database URL from environment variables
-DATABASE_URL = os.getenv("DB_URL")
-# DATABASE_URL = "mssql+pyodbc://sa:Fyppear@2@10.96.188.171:1433/user_service_dev"
-    "?TrustServerCertificate=yes&driver=ODBC+Driver+17+for+SQL+Server"
+#ntu
+#DATABASE_URL = os.getenv("DB_URL")
+#local
+DATABASE_URL = os.getenv("DB_URL_LOCAL")
+
+# DATABASE_URL = ("mssql+pyodbc://sa:Fyppear@2@10.96.188.171:1433/user_service_dev"
+# "?TrustServerCertificate=yes&driver=ODBC+Driver+17+for+SQL+Server")
+
 print(DATABASE_URL, "Trying...")
 # Create engine for MSSQL database
 engine = create_engine(DATABASE_URL)
@@ -20,6 +25,18 @@ engine = create_engine(DATABASE_URL)
 # Create all tables in the database
 try:
     with engine.connect() as connection:
+        Base.metadata.create_all(bind=engine)
         print("Connection successful!")
 except Exception as e:
     print(f"Connection failed: {e}")
+    
+
+# Create engine for MSSQL database
+engine = create_engine(DATABASE_URL)
+
+# Create all tables in the database
+try:
+    Base.metadata.create_all(bind=engine)
+    print("Tables created successfully.")
+except ProgrammingError as e:
+    print(f"An error occurred: {e}")
