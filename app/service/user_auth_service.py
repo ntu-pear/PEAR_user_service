6adcfbf61c0d9ee8b48568d66b47bde71908ec90
LAPTOP-J191SCGH\nat <nati.saver@gmail.com>
2025-01-24 23:47:11 +0800

    fix error handling and jwt

--- app/service/user_auth_service.py
+++ app/service/user_auth_service.py
@@ -12,13 +12,50 @@ import os
 import logging
 import json
 
+# Setup Variables
 SECRET_KEY=os.getenv('SECRET_KEY')
+if not SECRET_KEY:
+    SECRET_KEY = "FakeKey"
+    logging.warning("SECRET_KEY environment variable not set. Using an insecure fallback for development.")
+    # raise ValueError("SECRET_KEY environment variable not set.")
+
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30  # Token validity period
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30)) # Token validity period
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/login")
 
+# Errors
+missing_field_exception = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Token is missing 'sub' claim field from JWT Token payload",
+    headers={"WWW-Authenticate": "Bearer"},
+)
+
+expired_token_exception = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Token has expired. Please log in again.",
+    headers={"WWW-Authenticate": "Bearer"},
+)
+
+invalid_token_exception = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED, 
+    detail="Could not validate token"
+)
+
+unknown_token_exception = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Unexpected error decoding token. Please provide a valid token.",
+)
+
+user_credentials_exception = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Could not validate credentials of user",
+    headers={"WWW-Authenticate": "Bearer"},
+)
+
+
+# Methods
 def get_password_hash(password):
     return pwd_context.hash(password)
 
@@ -37,38 +74,30 @@ def create_access_token(data: dict, expires_delta: timedelta = None):
     return encoded_jwt
 
 def decode_access_token(token: str):
+    # extracts user details from jwt token
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
-        data = payload.get("sub")
-        userDetails = json.loads(data)
-
-        if userDetails is None:
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
-        
-        return userDetails
-    except JWTError:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate token")
-
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_details = json.loads(payload.get("sub"))
+        if not user_details:
+            raise missing_field_exception
+        return user_details
+    # deal with invalid token/expired token
+    except JWTError as e:
+        if "Signature has expired" in str(e):
+            raise expired_token_exception
+        else:
+            raise invalid_token_exception
+    # deal with else case for errors
+    except Exception as e:
+        logging.error(f"Unexpected token decoding error: {e}")
+        raise unknown_token_exception
 
 def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
-        data=payload.get("sub")
-        data=json.loads(data)
-        print(data)
-        userDetails=data 
-        if userDetails is None:
-            raise credentials_exception
-    except JWTError:
-        raise credentials_exception
-
+    # get user details from access token
+    userDetails = decode_access_token(token)
     user = db.query(User).filter(User.id == userDetails["userId"]).first()
-  
-    if userDetails is None:
-        raise credentials_exception
-    return {"userId":user.id, "fullName": user.nric_FullName, "roleName":user.roleName}
+
+    if not user:
+        raise user_credentials_exception
+
+    return {"userId": user.id, "fullName": user.nric_FullName, "roleName": user.roleName}
