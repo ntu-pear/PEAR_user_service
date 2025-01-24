126ff08edca0ce83a431dddb1c2d5704f66bc9c6
LAPTOP-J191SCGH\nat <nati.saver@gmail.com>
2025-01-24 23:59:05 +0800

    updated user_auth_router

--- app/routers/user_auth_router.py
+++ app/routers/user_auth_router.py
@@ -12,26 +12,37 @@ from ..service import user_auth_service
 from ..schemas import user_auth
 
 
-
 router = APIRouter()
-
 
 @router.post("/login/")#, response_model=user_auth.Token)
 def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
-    #Get User
+    # Get User
     user = db.query(User).filter(User.email == form_data.username).first()
     
+    # Validate User Credentials
     if not user:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="The user does not exist")
+        raise user_auth_service.user_credentials_exception
     if not user_auth_service.verify_password(form_data.password, user.password):
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
+        raise user_auth_service.user_credentials_exception
     
-    data={"userId":user.id,"fullName": user.nric_FullName, "roleName": user.roleName}
-    #Convert to JSON
-    data=json.dumps(data)
-    #Add data into access token
-    access_token = user_auth_service.create_access_token(data={"sub": data})
-    return {"access_token": access_token, "token_type": "bearer", "data":data}
+    # Prepare token payload
+    data = {
+        "userId": user.id,
+        "fullName": user.nric_FullName,
+        "roleName": user.roleName,
+    }
+
+    # Serialize payload and generate access token
+    serialized_data = json.dumps(data)
+    access_token = user_auth_service.create_access_token(data={"sub": serialized_data})
+    # Return response
+    return {
+        "access_token": access_token,
+        "token_type": "bearer",
+        # Include token expiry information in the response for the client to handle reauthentication.
+        "expires_in": user_auth_service.ACCESS_TOKEN_EXPIRE_MINUTES,
+        "data": data,  # Avoid sensitive data
+    }
 
 @router.get("/current_user/", response_model=user_auth.TokenData)
 def read_current_user(current_access: user_auth.TokenData = Depends(user_auth_service.get_current_user)):
