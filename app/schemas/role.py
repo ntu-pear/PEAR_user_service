b0a57c7fefe2087c09bcd2a1148c89ac8cbdc21c
Cheyenne Seet <seet.tzeshin.cheyenne@gmail.com>
2026-03-21 19:28:23 +0800

    remove hardcoded access level and use accesslevelid to reference accesslevel fir each role

--- app/schemas/role.py
+++ app/schemas/role.py
@@ -1,40 +1,44 @@
 from pydantic import BaseModel
 from datetime import datetime
-from typing import Optional
-from app.models.role_model import RolePrivacyStatus
-from typing import List
+from typing import Optional, List
+from .access_level import AccessLevelRead
+
 class RoleBase(BaseModel):
     roleName: str
-    accessLevelSensitive: RolePrivacyStatus
+    description: Optional[str] = None
+    accessLevelId: str
+
+class RoleCreate(RoleBase):
+    pass
 
 class RoleUpdate(BaseModel):
     roleName: Optional[str] = None
+    description: Optional[str] = None
+    accessLevelId: Optional[str] = None
     isDeleted: Optional[bool] = None
-    accessLevelSensitive: Optional[RolePrivacyStatus] = None
 
 class RoleRead(RoleBase):
     id: str
     isDeleted: bool
-    accessLevelSensitive: RolePrivacyStatus
-    createdById: Optional[str]=None
+    createdById: Optional[str] = None
     createdDate: datetime
-    modifiedById: Optional[str]=None
+    modifiedById: Optional[str] = None
     modifiedDate: datetime
+    accessLevel: Optional[AccessLevelRead] = None
+
     class Config:
         orm_mode = True
-        json_encoders = {
-            datetime: lambda v: v.strftime('%Y-%m-%d %H:%M:%S') if v else None
-        }
+
+class RoleNameRead(BaseModel):
+    roleName: str
 
 class RolePaginationResponse(BaseModel):
     total: int
     page: int
     page_size: int
-    class Config:
-        orm_mode = True
 
 class AdminRolePaginationResponse(RolePaginationResponse):
     roles: List[RoleRead]
 
 class RoleNamePaginationResponse(RolePaginationResponse):
-    roles: List[RoleBase] #return name and privacylevel
+    roles: List[RoleNameRead]
