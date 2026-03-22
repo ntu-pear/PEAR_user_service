55474fce96566d4f664e8811a54123e0c880f466
Cheyenne Seet <seet.tzeshin.cheyenne@gmail.com>
2026-03-22 11:33:06 +0800

    update routing endpoints

--- app/routers/access_level_router.py
+++ app/routers/access_level_router.py
@@ -8,16 +8,21 @@ from ..schemas.access_level import AccessLevelCreate, AccessLevelUpdate, AccessL
 from ..schemas import user_auth
 from ..service import user_auth_service as AuthService
 
-router = APIRouter(prefix="/access-levels", tags=["Access Levels"])
+router = APIRouter(
+    tags=["access_levels"],
+    dependencies=[Depends(get_db)],
+    responses={404: {"description": "Not found"}},
+)
 
-@router.get("/", response_model=list[AccessLevelRead])
+
+@router.get("/access_levels", response_model=list[AccessLevelRead])
 def read_access_levels(
     db: Session = Depends(get_db),
     current_user: user_auth.TokenData = Depends(AuthService.get_current_user)
 ):
     return access_level_crud.get_all_access_levels(db)
 
-@router.get("/{access_level_id}", response_model=AccessLevelRead)
+@router.get("/access_levels/{access_level_id}", response_model=AccessLevelRead)
 def read_access_level(
     access_level_id: str,
     db: Session = Depends(get_db),
@@ -28,7 +33,7 @@ def read_access_level(
         raise HTTPException(status_code=404, detail="Access level not found.")
     return db_obj
 
-@router.post("/create", response_model=AccessLevelRead)
+@router.post("/access_levels/create", response_model=AccessLevelRead)
 def create_new_access_level(
     data: AccessLevelCreate,
     db: Session = Depends(get_db),
@@ -45,7 +50,7 @@ def create_new_access_level(
         created_by=current_user["userId"]
     )
 
-@router.put("/update/{access_level_id}", response_model=AccessLevelRead)
+@router.put("/access_levels/update/{access_level_id}", response_model=AccessLevelRead)
 def update_existing_access_level(
     access_level_id: str,
     data: AccessLevelUpdate,
@@ -66,7 +71,7 @@ def update_existing_access_level(
         modified_by=current_user["userId"]
     )
 
-@router.delete("/delete/{access_level_id}", response_model=AccessLevelRead)
+@router.delete("/access_levels/delete/{access_level_id}", response_model=AccessLevelRead)
 def delete_existing_access_level(
     access_level_id: str,
     db: Session = Depends(get_db),
